3ad1d601e2421f170ba657fa48d3fcdb23f339a5
Getmeoutt <noppanatsri@gmail.com>
2025-01-10 00:52:36 -0800

    add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,69 @@
-import Image from "next/image";
+"use client";
+import React from "react";
+import {FloatingNav} from "@/components/ui/floating-navbar";
+import { motion } from "framer-motion";
+import { Typewriter } from "react-simple-typewriter";
+import { Fade } from "react-awesome-reveal";
+import { Feather } from "lucide-react";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+   
+        
+    <div className="relative w-full h-screen bg-white">
+      <FloatingNav
+        navItems={[
+          {
+            name: "Home",
+            link: "#home",
+          },
+          
+          {
+            name: "project",
+            link: "#projects",
+          },
+          {
+            name: "Contact",
+            link: "#contact",
+          }
+        ]}/>    
+      <div className="absolute top-1/4 right-48">
+      <h1 className=" text-6xl font-bold">Hi there! <br></br><div className="flex text-2xl"><p > I'm</p> <p className="text-violet-900 ml-1"><Typewriter  words={[" Tom"," technology enthusiast"]} cursor={true} loop={false}></Typewriter></p><br></br></div><p className="text-xl font-extralight">A current student at BCIT<br></br> in the Computer Information Technology program</p></h1>
+      <div className="skills flex flex-wrap space-x-2 px-1">
+      <div className="s">python</div> 
+      <div className="s">javascript</div> 
+      <div className="s">networking</div> 
+      <div className="s">linux</div> 
+      <div className="w-full"></div> {/* Break line here */}
+      <div className="s">html</div>
+      <div className="s">css</div> 
+      <div className="s">sql</div> 
+      <div className="s">R</div>
+    </div>
+      </div>
+      <div className="container relative w-full h-full m-0">
+        <div className="triangle absolute bottom-0 left-0 w-full h-[30vh] bg-slate-700 z-10" style={{ clipPath: "polygon(100% 100%, 0% 100%, 0 0%)" }}></div>
+           <div className="profile absolute top-14 right-72 ">
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      <div id="home"></div>
+      </div>
+      
+      <h1 className="my-10 text-4xl m-10" id="projects">Projects</h1>
+      <Fade>
+        <div className="flex gap-4 mx-10 flex-row flex-wrap self-center ">
+          <Card title={"Discord Bot project"} picture={"img/discord_project.png"} description={"ahdsfjksdfhj sd hfjksdfhj dsjkf hskdjf df jshdfjk d djfhjsdkfh"}></Card>
+          <Card title={"Discord Bot project"} picture={"img/discord_project.png"} description={"ahdsfjksdfhj sd hfjksdfhj dsjkf hskdjf df jshdfjk d djfhjsdkfh "} link={"https://github.com/"}></Card>
+          <Card title={"Discord Bot project"} picture={"img/discord_project.png"} description={"ahdsfjksdfhj sd hfjksdfhj dsjkf hskdjf df jshdfjk d djfhjsdkfh "} link={"https://github.com/"}></Card>
+          <Card title={"Discord Bot project"} picture={"img/discord_project.png"} description={"ahdsfjksdfhj sd hfjksdfhj dsjkf hskdjf df jshdfjk d djfhjsdkfh "} link={"https://github.com/"}></Card>
+          <Card title={"Discord Bot project"} picture={"img/discord_project.png"} description={"ahdsfjksdfhj sd hfjksdfhj dsjkf hskdjf df jshdfjk d djfhjsdkfh "} link={"https://github.com/"}></Card>
+        </div>
+      </Fade>
     </div>
   );
-}
+};
+
+
